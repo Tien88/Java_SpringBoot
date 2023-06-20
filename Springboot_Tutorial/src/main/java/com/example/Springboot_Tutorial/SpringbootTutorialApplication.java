2f52c42031ad0a1741a67124bb2f50c1518d13f2
Tien <tmtien0103@gmail.com>
2023-06-20 14:52:39 +0700

    first commit

---
+++ Springboot_Tutorial/src/main/java/com/example/Springboot_Tutorial/SpringbootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialApplication.class, args);
+	}
+
+}
---
+++ Springboot_Tutorial/src/test/java/com/example/Springboot_Tutorial/SpringbootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
